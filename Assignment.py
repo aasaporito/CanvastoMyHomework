9e678bd3a59d9cbcd8965cdd9f64df123f43474a
Aaron Saporito <sapblatt11@gmail.com>
2019-01-23 21:30:35 -0500

    Adds Assignments to myHomework

--- Assignment.py
+++ Assignment.py
@@ -4,4 +4,5 @@ class Assignment:
     def __init__(self, name="No Name", course="", dueAt= "1/1/2019"):
         self.name = name
         self.course = course
-        self.dueAt = datetime.strftime(datetime.strptime(dueAt, "%Y-%m-%dT%H:%M:%S%z") - timedelta(days=1), "%m/%d/%Y")
+        #date = datetime.strptime(dueAt, "%Y-%m-%dT%H:%M:%S%z") - timedelta(days=1)
+        self.dueAt = datetime.strftime(dueAt, "%m/%d/%Y")
--- canvastoMyHW.py
+++ canvastoMyHW.py
@@ -2,6 +2,11 @@
 from canvasapi import Canvas
 from Assignment import Assignment
 import json
+from splinter import Browser
+import time
+from datetime import datetime, timedelta
+import pytz
+
 
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -21,7 +26,39 @@ courses = canvas.get_user(1425).get_courses()
 
 for course in courses:
 	for assignment in course.get_assignments():
-		assignments.append(Assignment(assignment.name, course.name, assignment.due_at))
+		date = datetime.strptime(assignment.due_at, "%Y-%m-%dT%H:%M:%S%z") - timedelta(days=1)
+		utc=pytz.UTC
+
+		
+		now = utc.localize(datetime.now())
+		if date >= now:
+			assignments.append(Assignment(assignment.name, course.name, date))
 
 
+
+username = config["username"]
+password = config["password"]
+b = Browser(driver_name="chrome", headless=False)
+b.driver.set_window_size(1400, 900)
+b.visit("https://myhomeworkapp.com/login")
+
+#Login
+b.fill("username", username)
+b.fill("password", password)
+b.find_by_value("Sign in").click()
+
+for a in assignments:
+	b.visit("https://myhomeworkapp.com/home")
+	time.sleep(2)
+	# if(b.is_text_present("Mark all of your late homework as complete?")):
+	# 	print("got here")
+	# 	b.find_by_text("Close").click()
+
+	b.find_by_xpath("//*[@id='main-content']/div[2]/div/a").click()
+	b.fill("title", a.name)
+	b.fill("due_date", a.dueAt)
+	
+	b.find_by_value("Save").click()
+	time.sleep(4)
+	print("Added: " + a.name)
 
