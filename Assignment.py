139a84c249f3bf72cfa122fb42722420b2328c8e
Aaron Saporito <sapblatt11@gmail.com>
2019-01-24 08:11:34 -0500

    Get Class List from myHomework to compare

--- Assignment.py
+++ Assignment.py
@@ -1,8 +1,11 @@
 from datetime import datetime, timedelta
+
+
 class Assignment:
-    
-    def __init__(self, name="No Name", course="", dueAt= "1/1/2019"):
+
+    def __init__(self, name="No Name", course="", dueAt="1/1/2019", description = ""):
         self.name = name
         self.course = course
         #date = datetime.strptime(dueAt, "%Y-%m-%dT%H:%M:%S%z") - timedelta(days=1)
         self.dueAt = datetime.strftime(dueAt, "%m/%d/%Y")
+        self.description = description
--- canvastoMyHW.py
+++ canvastoMyHW.py
@@ -6,12 +6,12 @@ from splinter import Browser
 import time
 from datetime import datetime, timedelta
 import pytz
-
+from lxml import html
 
 with open('config.json') as config_file:
     config = json.load(config_file)
 
-#SequenceMatcher(None, "AP Computer Science A", "AP Comp Sci" ).ratio())
+# SequenceMatcher(None, "AP Computer Science A", "AP Comp Sci" ).ratio())
 
 assignments = []
 # Canvas API URL
@@ -25,16 +25,15 @@ canvas = Canvas(API_URL, API_KEY)
 courses = canvas.get_user(1425).get_courses()
 
 for course in courses:
-	for assignment in course.get_assignments():
-		date = datetime.strptime(assignment.due_at, "%Y-%m-%dT%H:%M:%S%z") - timedelta(days=1)
-		utc=pytz.UTC
+    for assignment in course.get_assignments():
+        date = datetime.strptime(
+            assignment.due_at, "%Y-%m-%dT%H:%M:%S%z") - timedelta(days=1)
+        utc = pytz.UTC
 
-		
-		now = utc.localize(datetime.now())
-		if date >= now:
-			assignments.append(Assignment(assignment.name, course.name, date))
-
-
+        now = utc.localize(datetime.now())
+        if date >= now:
+            assignments.append(Assignment(
+                assignment.name, course.name, date, assignment.description))
 
 username = config["username"]
 password = config["password"]
@@ -42,23 +41,31 @@ b = Browser(driver_name="chrome", headless=False)
 b.driver.set_window_size(1400, 900)
 b.visit("https://myhomeworkapp.com/login")
 
-#Login
+# Login
 b.fill("username", username)
 b.fill("password", password)
 b.find_by_value("Sign in").click()
 
+# Get Classes
+page = b.html
+page = ' '.join(page.split('\n'))
+tree = html.fromstring(page)
+
+classes = tree.xpath("//*[@id='main-content']/div[1]/div/ul/li/a/text()")[:7]
+
+
 for a in assignments:
-	b.visit("https://myhomeworkapp.com/home")
-	time.sleep(2)
-	# if(b.is_text_present("Mark all of your late homework as complete?")):
-	# 	print("got here")
-	# 	b.find_by_text("Close").click()
+    b.visit("https://myhomeworkapp.com/home")
+    time.sleep(2)
+    # if(b.is_text_present("Mark all of your late homework as complete?")):
+    #   print("got here")
+    #   b.find_by_text("Close").click()
 
-	b.find_by_xpath("//*[@id='main-content']/div[2]/div/a").click()
-	b.fill("title", a.name)
-	b.fill("due_date", a.dueAt)
-	
-	b.find_by_value("Save").click()
-	time.sleep(4)
-	print("Added: " + a.name)
+    b.find_by_xpath("//*[@id='main-content']/div[2]/div/a").click()
+    b.fill("title", a.name)
+    b.fill("due_date", a.dueAt)
+    b.fill("additional_info", a.description)
 
+    b.find_by_value("Save").click()
+    time.sleep(4)
+    print("Added: " + a.name)
